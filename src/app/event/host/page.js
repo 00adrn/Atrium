5e8a9f55440c101082d264d8d105633704da0a06
00adrn <adrianvez11@gmail.com>
2026-01-24 11:22:52 -0500

    Set up event creation and join functionality.

--- src/app/event/host/page.js
+++ src/app/event/host/page.js
@@ -2,17 +2,41 @@
 "use client"
 import { useSearchParams } from "next/navigation"
 import { createClient } from 'lib/supabase/client'
+import { useState, useEffect } from "react"
+
 
 export default function Page () {
     const supabase = createClient();
     const searchParams = useSearchParams();
+
     const eventId = searchParams.get("eventId");
-    const joinCode = searchParams.get("joinCode");
+
+    const [eventName, seteventName] = useState(null);
+    const [joinCode, setjoinCode] = useState(null);
+    const [joinUrk, setJoinUrl] = useState(null);
+
+    useEffect(() => {
+        getEvent();
+    }, []);
+
+    const getEvent = async () => {
+        const { data, error } = await supabase.from("events").select().eq("id", eventId).single();
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        seteventName(data.name);
+        setjoinCode(data.join_code);
+        setlinks((data.links).split("|"));
+    }
 
     return (
         <div>
             <h1>EventID: {eventId}</h1>
             <h1>JoinCode: {joinCode}</h1>
+            <h1>EventName: {eventName}</h1>
         </div>
     );
 }
--- src/app/event/join/page.js
+++ src/app/event/join/page.js
@@ -1,15 +1,42 @@
 "use client"
 import { useSearchParams } from "next/navigation"
 import { createClient } from 'lib/supabase/client'
+import { useState, useEffect } from "react"
 
 export default function Page () {
     const supabase = createClient();
+
+
     const searchParams = useSearchParams();
-    const joinCode = searchParams.get("joinCode");
+    const joinCode = searchParams.get("code");
+
+    const [eventName, seteventName] = useState(null);
+    const [links, setlinks] = useState([]);
+
+    useEffect(() => {
+        fetchEventData();
+    }, []);
+
+
+    const fetchEventData = async () => {
+        const { data, error } = await supabase.from("events").select().eq("join_code", joinCode).single();
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        seteventName(data.name);
+        setlinks(data.links.split("|"));
+    }
 
     return (
         <div>
-            <h1>JoinCode: {joinCode}</h1>
+            <h1>Event Name: {eventName}</h1>
+            <h1>Links</h1>
+            {links.map((link, i) => {
+                return <p key={i}><a href={link}>{link}</a></p>
+            })}
         </div>
     );
 }
