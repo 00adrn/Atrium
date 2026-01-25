1ae53c13acbeb94ee36c127af2d2ad81338317db
mateomcallister <mateomcallisterschool@gmail.com>
2026-01-25 01:33:11 -0500

    Added functionality to profile picture in navbar

--- src/app/dashboard/Navbar.js
+++ src/app/dashboard/Navbar.js
@@ -2,10 +2,40 @@
 
 import { redirect } from "next/navigation"
 import { AnimatePresence, motion } from "framer-motion"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { createClient } from "lib/supabase/client"
 
 const Navbar = ({ headshot }) => {
   const [pfpMenuOpen, setPfpMenuOpen] = useState(false)
+  const [userName, setUserName] = useState("")
+  const [userDesc, setUserDesc] = useState("")
+  const supabase = createClient()
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const { data: { user: userRes } } = await supabase.auth.getUser()
+
+      if (userRes) {
+        const { data, error } = await supabase.from('users').select().eq('id', userRes.id).single()
+        console.log(data)
+        if (error) console.log(error)
+        
+        setUserName(`${data.first_name} ${data.last_name}`)
+        setUserDesc(data.description)
+      }
+    }
+    
+    getUserData()
+  }, [])
+
+  const handleClick = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.log(error)
+    } else {
+      redirect("/login")
+    }
+  }
 
   return (
     <nav className="fixed top-0 w-full bg-white border-b border-gray-200 px-4 py-2 z-10">
@@ -24,13 +54,19 @@ const Navbar = ({ headshot }) => {
         <AnimatePresence initial={false} mode="wait">
           {pfpMenuOpen && 
               <motion.div 
-                className="absolute top-11 right-4 w-36 h-56 bg-white rounded-lg border border-gray-200 shadow-md"
+                className="absolute top-11 right-4 w-48 h-fit p-2 bg-white rounded-lg border border-gray-200 shadow-md flex flex-col gap-4"
                 initial={{ opacity: 0, scale: 0.95 }}
                 animate={{ opacity: 1, scale: 1.0 }}
                 exit={{ opacity: 0, scale: 0.95 }}
                 transition={{ duration: 0.1 }}
               >
-                
+                <div>
+                  <p className="text-black">{userName}</p>
+                  <p className="text-gray-500">{userDesc}</p>
+                </div>
+                <button onClick={() => handleClick()} className='w-full text-black hover:cursor-pointer mt-auto h-8 rounded-sm bg-red-500/20 hover:bg-red-500/40 transition-colors duration-200'>
+                  Sign out
+                </button>
               </motion.div>
           }
         </AnimatePresence>
